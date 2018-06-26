c9c17677b95ec11caeaead00a277221b3186a888
Gaddam <vgaddam@res1vgaddam-m1.vcorp.ad.vrsn.com>
2018-06-26 13:02:12 -0400

    Solution to water trapping problem in O(n)

---
+++ trappedWater.py
@@ -0,0 +1,28 @@
+def trappedWater(brickHeight):
+	maxLeftHeight = []
+	maxRightHeight = []
+	reversedList = brickHeight[::-1]
+	trappedWater = 0
+
+	maxLeftHeight.append(-1)
+	maxLeftHeight.append(brickHeight[0])
+	maxRightHeight.append(-1)
+	maxRightHeight.append(reversedList[0])
+	for i in range(2,len(brickHeight)):
+		
+		maxBrickHeight = max(brickHeight[i-1],maxLeftHeight[i-1])
+		maxLeftHeight.append(maxBrickHeight)
+
+	for i in range(2,len(brickHeight)):
+		
+		maxRightBrickHeight = max(reversedList[i-1],maxRightHeight[i-1])
+		maxRightHeight.append(maxRightBrickHeight)
+	
+	trueRightHeight = maxRightHeight[::-1]
+	for k in range(1,len(brickHeight)-1):
+		trappedWater += min(maxLeftHeight[k],trueRightHeight[k]) - brickHeight[k]
+	return trappedWater
+
+tester = [3,0,0,2,0,4]
+print("The maximum amount of water that can be trapped will be " + str(trappedWater(tester)) + " gallons of water")
+
